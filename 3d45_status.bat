f7b23fee9ebea20044d94afb420e123afd724d7f
janitz <janis-d@web.de>
2020-04-26 11:05:02 +0200

    added 3d45_status script (win)

---
+++ 3d45_status.bat
@@ -0,0 +1,2 @@
+curl 192.168.0.26/command -X POST -H "Content-Type: application/x-form-urlencoded" -d "GETPRINTERSTATUS" > %USERPROFILE%\Desktop\3d45_status.json
+@echo. >> %USERPROFILE%\Desktop\3d45_status.json
